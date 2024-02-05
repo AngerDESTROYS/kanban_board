884a0f70049c20d7b23d8d0a20fbc127546f603b
AngerDESTROYS <Nehrim24@gmail.com>
2024-02-05 14:37:43 +0200

    add task solution

--- src/api/api.ts
+++
@@ -1,6 +0,0 @@
-// api.ts
-const GITHUB_API_BASE_URL = 'https://api.github.com';
-
-export const fetchIssues = async (repoUrl: string) => {
-  // Implement GitHub API request to fetch issues based on the repo URL
-};
